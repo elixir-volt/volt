b754bd586a0490699af24dbfe66c4b7e424cb551
Danila Poyarkov <dev@dannote.net>
2026-06-27 11:50:51 +0200

    Dogfood Volt JavaScript tool config

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,16 @@
 import Config
 
+config :volt, :format,
+  trailing_comma: :none,
+  tab_width: 2,
+  semi: false,
+  single_quote: true,
+  print_width: 100,
+  arrow_parens: :always
+
+config :volt, :lint,
+  rules: %{
+    "no-unused-expressions" => :allow
+  }
+
 import_config "#{config_env()}.exs"
