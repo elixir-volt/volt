ad18fae89eee53ab73deb84f13070088e91f38b8
Danila Poyarkov <dev@dannote.net>
2026-07-06 11:26:03 +0200

    Use JSONCodec for console forwarding payloads

--- lib/volt/dev/console_forwarder.ex
+++ lib/volt/dev/console_forwarder.ex
@@ -6,6 +6,20 @@ defmodule Volt.Dev.ConsoleForwarder do
   @endpoint "/@volt/console"
   @support_modules {:volt, "ts"}
 
+  defmodule Payload do
+    @moduledoc false
+
+    use JSONCodec, fast_path: :json
+
+    defstruct [:level, :source, :args]
+
+    @type t :: %__MODULE__{
+            level: :log | :info | :warn | :error | :debug,
+            source: String.t(),
+            args: [term()]
+          }
+  end
+
   @spec endpoint() :: String.t()
   def endpoint, do: @endpoint
 
@@ -14,40 +28,32 @@ defmodule Volt.Dev.ConsoleForwarder do
     Volt.Priv.js!(@support_modules, "dev/console-forwarder.ts") <> "\n" <> code
   end
 
-  @spec log(binary() | map()) :: :ok
+  @spec log(binary() | Payload.t()) :: :ok
   def log(payload) when is_binary(payload) do
-    case Jason.decode(payload) do
-      {:ok, map} when is_map(map) -> log(map)
-      _ -> :ok
+    case Payload.decode(payload) do
+      {:ok, payload} -> log(payload)
+      {:error, _reason} -> :ok
     end
   end
 
-  def log(%{"level" => level, "args" => args, "source" => source}) when is_list(args) do
+  def log(%Payload{level: level, args: args, source: source}) do
     message = Enum.map_join(args, " ", &format_arg/1)
-    prefix = if source in [nil, ""], do: "[Volt][browser]", else: "[Volt][browser][#{source}]"
+    prefix = browser_prefix(source)
 
-    case normalize_level(level) do
-      :error -> Logger.error("#{prefix} #{message}")
-      :warning -> Logger.warning("#{prefix} #{message}")
-      :info -> Logger.info("#{prefix} #{message}")
-      :debug -> Logger.debug("#{prefix} #{message}")
-    end
-
-    :ok
+    log_message(level, "#{prefix} #{message}")
   end
 
   def log(_), do: :ok
 
+  defp browser_prefix(""), do: "[Volt][browser]"
+  defp browser_prefix(source), do: "[Volt][browser][#{source}]"
+
   defp format_arg(arg) when is_binary(arg), do: arg
   defp format_arg(arg), do: inspect(arg, pretty: false, limit: :infinity)
 
-  defp normalize_level("error"), do: :error
-  defp normalize_level(:error), do: :error
-  defp normalize_level("warn"), do: :warning
-  defp normalize_level(:warn), do: :warning
-  defp normalize_level("info"), do: :info
-  defp normalize_level(:info), do: :info
-  defp normalize_level("debug"), do: :debug
-  defp normalize_level(:debug), do: :debug
-  defp normalize_level(_), do: :info
+  defp log_message(:error, message), do: Logger.error(message)
+  defp log_message(:warn, message), do: Logger.warning(message)
+  defp log_message(:log, message), do: Logger.info(message)
+  defp log_message(:info, message), do: Logger.info(message)
+  defp log_message(:debug, message), do: Logger.debug(message)
 end
